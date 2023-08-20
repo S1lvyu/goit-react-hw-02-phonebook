331e6865b3f64746c173692a8cd3034dccd0754b
Silvian Ranetescu <silvian.ranetescu@gmail.com>
2023-08-20 13:04:54 +0300

    hw done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+import React, { useState } from 'react';
+
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import ContactFilter from './ContactFilter/ContactFilter';
+import Section from './Section/Section';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  const [filter, setFilter] = useState('');
+  const onDelete = contactToDelete => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactToDelete.id)
+    );
+    setFilter('');
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Section title="Phonebook">
+        <ContactForm contacts={contacts} setContacts={setContacts} />
+      </Section>
+      <Section title="Contacts">
+        <ContactFilter filter={filter} setFilter={setFilter} />
+        <ContactList contacts={contacts} filter={filter} onDelete={onDelete} />
+      </Section>
     </div>
   );
 };
